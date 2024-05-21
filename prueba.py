35ebe97163c4ce79d91dedb8d51e9a4106895c6b
CNath01 <92951005+CNath01@users.noreply.github.com>
2024-05-21 10:28:32 -0500

    Create prueba.py

---
+++ prueba.py
@@ -0,0 +1,10 @@
+import random
+my_words = ["HI", "Python Pro", "Kodland"] 
+print(my_words[1])
+name = input("Ingrese su nombre")
+edad = int(input("Ingrese su edad"))
+print("Hi ", name)
+emojis = ["^^", "0_o", ":)", "¯\\_(ツ)_/¯", "(￢_￢)"]
+print(random.choice(emojis))
+yo = ["me gusta la papa", "me gusta pintar"]
+print(random.choice(yo))
